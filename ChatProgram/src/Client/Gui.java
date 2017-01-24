7be888ae3ed900c2d3788ff16ac1197abdc4c41c
wjdcks725 <wjdcks725@naver.com>
2017-01-24 16:54:45 +0900

    스크롤바 항상 textarea하단 위치하도록 추가

--- ChatProgram/src/Client/Gui.java
+++ ChatProgram/src/Client/Gui.java
@@ -1,29 +1,34 @@
 package Client;
 
+import java.awt.Adjustable;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.text.DefaultCaret;
 
 public class Gui {
 	private static JFrame mainFrame;
 	private static JPanel chatPane;
 	private static JTextArea chatText;
 	private static JTextField chatLine;
-	
+
 	private SendMessageHandler sendHandler;
 	private ReceiveMessageHandler receiveHandler;
-	
+
 	private Thread receiveThread;
 	private Thread sendThread;
-	
+
 	public String getChatText() {
 		return chatText.getText();
 	}
@@ -31,7 +36,7 @@ public class Gui {
 	public static String getChatLine() {
 		return chatLine.getText();
 	}
-	
+
 	public static void setChatText(String text) {
 		chatText.setText(text);
 	}
@@ -39,14 +44,14 @@ public class Gui {
 	public static void setChatLine(String text) {
 		chatLine.setText(text);
 	}
-	
+
 	public Gui(ObjectInputStream readStream, ObjectOutputStream writeStream) {
 		this.sendHandler = new SendMessageHandler(writeStream, this);
 		this.receiveHandler = new ReceiveMessageHandler(readStream, this);
-		
+
 		initChatRoom();
 	}
-	
+
 	public void initChatRoom() {
 		chatPane = new JPanel(new BorderLayout());
 		chatText = new JTextArea(10, 20);
@@ -54,12 +59,16 @@ public class Gui {
 		chatText.setEditable(false); // textbox 수정여부
 		chatText.setForeground(Color.blue); // 글씨색
 		
-		JScrollPane chatTextPane = new JScrollPane(chatText, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		DefaultCaret caret = (DefaultCaret) chatText.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);	// 스크롤바가 항상 textarea 하단에 위치 
 
+		 JScrollPane chatTextPane = new JScrollPane(chatText,
+		 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+		 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		 
 		chatLine = new JTextField();
 		chatLine.setEnabled(true);
-		chatLine.addKeyListener(this.sendHandler);		// 키이벤트
+		chatLine.addKeyListener(this.sendHandler); // 키이벤트
 
 		chatPane.add(chatLine, BorderLayout.SOUTH);
 		chatPane.add(chatTextPane, BorderLayout.CENTER);
@@ -68,7 +77,7 @@ public class Gui {
 		JPanel mainPane = new JPanel(new BorderLayout());
 		mainPane.add(chatPane, BorderLayout.CENTER);
 
-		mainFrame = new JFrame("Multi Chatting");
+		mainFrame = new JFrame("채팅 프로그램");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setContentPane(mainPane);
 		mainFrame.setSize(mainFrame.getPreferredSize());
@@ -76,22 +85,22 @@ public class Gui {
 		mainFrame.pack();
 		mainFrame.setVisible(true);
 	}
-	
+
 	public void createThread() {
 		sendThread = new Thread(this.sendHandler);
 		receiveThread = new Thread(this.receiveHandler);
 	}
-	
+
 	public void startThread() {
 		receiveThread.start();
 		sendThread.start();
-		
+
 		try {
 			receiveThread.join();
-			sendThread.join(); 
+			sendThread.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
